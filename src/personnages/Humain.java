307b7d60088579aba6500de5f53f96bfebda0985
ivanu <ivan-mauricio.uribe-carrillo@univ-tlse3.fr>
2022-10-27 11:42:46 +0400

    PierreSabre

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -3,25 +3,46 @@ package personnages;
 public class Humain {
 	private String nom;
 	private String boissonPreferee;
-	private int argentPerso;
+	private int sous;
 	
-	public Humain(String nom, String boissonPreferee, int argentPerso) {
+	public Humain(String nom, String boissonPreferee, int sous) {
 		this.nom = nom;
 		this.boissonPreferee = boissonPreferee;
-		this.argentPerso = argentPerso;
+		this.sous = sous;
 	}
-	public String getNom() {
-		return nom;
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "«" + texte + "»");
+	}
+	
+	private String prendreParole() {
+		return "(" + nom + ") - ";
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m’appelle " + this.nom + " et j’aime boire du " + this.boissonPreferee) ;
 	}
 
-
-	public String getBoissonPreferee() {
-		return boissonPreferee;
+	public void boire() {
+		parler("Mmmm, un bon verre de " +  this.boissonPreferee + "! GLOUPS !");
 	}
-
-
-	public int getArgentPerso() {
-		return argentPerso;
+	
+	public int gaganerArgent(int gain) {
+		return this.sous+=gain;
+	}
+	
+	public int perdreArgent(int perte) {
+		return this.sous-=perte;
+	}
+	
+	public void acheter(String bien, int prix) {
+		if(this.sous<prix) {
+			parler("Je n'ai plus que " + this.sous + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix + " sous.");
+		}
+		else{
+			parler("J'ai " + this.sous + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous.");
+			perdreArgent(prix);
+		}
 	}
 
 
