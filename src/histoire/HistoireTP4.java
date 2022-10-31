798f7800a9efe84c6f585747142357d1ab25a237
ivanu <ivan-mauricio.uribe-carrillo@univ-tlse3.fr>
2022-10-31 14:23:26 +0400

    PierreSabre

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,14 +1,15 @@
 package histoire;
 import personnages.Humain;
+import personnages.Humain.Commercant;
 public class HistoireTP4 {
 
 	public static void main(String[] args) {
-		Humain prof = new Humain("Prof","kombucha",54);
-		prof.direBonjour();
-		prof.acheter("une boisson",12);
-		prof.boire();
-		prof.acheter("un jeu",2);
-		prof.acheter("un kimono",50);
+		Humain commercant = new Humain("Commercant","Thé",20);
+		Commercant marco = commercant.new Commercant("Marco","Thé",20);
+		marco.direBonjour();
+		marco.seFaireExtorquer();
+		marco.recevoir(15);
+		marco.boire();
 	}
 
 }
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -45,9 +45,36 @@ public class Humain {
 		}
 	}
 
-
+	public class Commercant extends Humain{
+		public Commercant(String nom, String boissonPreferee, int sous) {
+			super(nom,boissonPreferee,sous);
+			super.nom = nom;
+			super.boissonPreferee = boissonPreferee;
+			super.sous = sous;
+		}
+		
+		//Si un field est marqué d'un accès privé, on ne peut y accéder qu'à 
+		//l'intérieur de cette classe ou de ses instances.
+		//Solution 1: Redéfinir les méthodes dans la classe fille Commerçant
+		//Solution 2: Faire appel au constructeur super()
+		
+		
+			public void seFaireExtorquer() {
+				super.sous-=super.sous;
+				super.parler("J’ai tout perdu! Le monde est trop injuste...");
+			}
+			
+			public void recevoir(int argent) {
+				super.sous+=argent;
+				super.parler(argent + " sous ! Je te remercie généreux donateur!");
+			}
+		}
+	
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
