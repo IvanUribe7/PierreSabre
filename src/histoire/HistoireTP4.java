55d808545850f4859fb799ccc8807de5de3e145b
ivanu <ivan-mauricio.uribe-carrillo@univ-tlse3.fr>
2022-10-31 15:28:52 +0400

    PierreSabre

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,17 +1,15 @@
 package histoire;
 import personnages.Humain;
 import personnages.Humain.Commerçant;
-import personnages.Humain.Yakuza;
+import personnages.Humain.Ronin;
 public class HistoireTP4 {
 
 	public static void main(String[] args) {
 		Humain humain = new Humain();
 		Commerçant marco = humain.new Commerçant("Marco","Thé",15);
-		Yakuza yaku = humain.new Yakuza("Yaku Le Noir","Whisky",30,"Hyrule",0);
-		yaku.direBonjour();
-		yaku.parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ?");
-		yaku.parler("Marco, si tu tiens à la vie donne moi ta bourse !");
-		yaku.extorquer(marco);
+		Ronin roro = humain.new Ronin("Roro","Shochu",60,1);
+		roro.direBonjour();
+		roro.donner(marco);
 	}
 
 }
